167105c977b6f5570f0fa904f73b2cb9765917a0
Oliver <olivercervantes2006@hotmail.com>
2026-02-12 17:43:03 -0600

    lo q quedaba creo?
     quedaba mas q hacer commity pero no se hizo lolol

--- Assets/Scripts/Player/InteractorTeclas.cs
+++ Assets/Scripts/Player/InteractorTeclas.cs
@@ -1,49 +1,12 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-////interface IInteractable
-////{
-////    public void Interact();
-////}
+public class InteractorTeclas : MonoBehaviour
+{
+   public void ActivarObjeto()
+   {
+        Destroy(gameObject);
+   }
 
-//public class InteractorTeclas : MonoBehaviour
-//{
-//    public Transform InteractorSource;
-//    public float interactRange;
-//    // Start is called once before the first execution of Update after the MonoBehaviour is created
-//    void Start()
-//    {
-
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (Input.GetKeyDown(KeyCode.I))
-//        {
-//            Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
-//            if (Physics.Raycast(r, out RaycastHit hitinfo, interactRange))
-//            {
-//                // Primero, intentar la interfaz genérica
-//                if (hitinfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
-//                {
-//                    interactObj.Interact();
-//                }
-
-//                // Fallback específico para cajas de munición tipo cofre
-//                var go = hitinfo.collider.gameObject;
-//                if (go.CompareTag("GunAmmo"))
-//                {
-//                    var box = go.GetComponent<AmmoBox>();
-//                    if (box != null)
-//                    {
-//                        GameManager.Instance.gunammo += box.ammo;
-//                        Destroy(go);
-//                    }
-//                }
-//            }
-//        }
-
-//    }
-//}
+}
